cdc03f6caaabe2d940735302c0b531a6bcd792d0
sandeepkaurs <sandeep.singh@telus.com>
2022-11-16 20:14:20 -0800

    Initial commit countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: " ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const result1 = countLetters("lighthouse in the house")
+console.log(result1)
