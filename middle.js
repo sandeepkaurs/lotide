df9aaeeadc44d842fc689d04c2b100d00cfa9958
sandeepkaurs <sandeep.singh@telus.com>
2022-11-22 04:22:22 -0800

    commit middle

--- middle.js
+++ middle.js
@@ -1,22 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i<array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   if (array.length < 3) {
@@ -31,5 +15,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([3, 2, 1]), [3]);
+module.exports = middle;
