d9f781f8a9f95d2c27873cd6acd10e050adb1275
sandeepkaurs <sandeep.singh@telus.com>
2022-11-22 04:02:59 -0800

    commit assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i<array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (!eqArrays(array1, array2)) {
@@ -18,3 +9,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
+module.exports = assertArraysEqual;
